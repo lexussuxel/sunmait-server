b224951244ce648b86d564442bb18f0e720b134d
Alexandra Danilevich <bloodangelalexandra@gmail.com>
2023-06-09 01:19:08 +0300

    Refactor

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,7 @@ const { LOGIN_MOCK } = require("./mockData.module");
 class UserController {
   authentification(req, res) {
     const { username, password } = req.body;
+
     if (username != LOGIN_MOCK.username && password != LOGIN_MOCK.password) {
       return res.json({ username });
     }
